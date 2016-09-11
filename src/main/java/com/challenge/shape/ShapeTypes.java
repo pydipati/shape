5f4ce16e2be4f9bf48d8e05a2bbe66eeb114c9c4
pydipati <karunak@yahoo.com>
2016-09-11 13:13:39 -0700

    Update ShapeTypes.java

--- src/main/java/com/challenge/shape/ShapeTypes.java
+++ src/main/java/com/challenge/shape/ShapeTypes.java
@@ -1,5 +1,12 @@
 package com.challenge.shape;
-
+/**
+* <h1>This is enum to support all possible shape types</h1>
+*  
+*
+* @author  Karuna P
+* @version 1.0
+* @since   2016-09-10 
+*/
 public enum ShapeTypes {
 	TRIANGLE, RECTANGLE;
 
