7d43d1ba2ddcdedfd7ed44e8ce9e707201b1fa9c
pydipati <karunak@yahoo.com>
2016-09-11 13:12:15 -0700

    Update Shape.java

--- src/main/java/com/challenge/shape/Shape.java
+++ src/main/java/com/challenge/shape/Shape.java
@@ -3,7 +3,7 @@ package com.challenge.shape;
 /**
 * <h1>This is super shape interface to support Triangle</h1>
 * <b>Note:</b> 
-* This is shape interface which supports all triangle types, all rectangles types and ellipse (circle & ellipse)
+* This is shape interface which supports all triangle types, all rectangles types and ellipse types (circle & ellipse)
 * <p>
 *  
 *
